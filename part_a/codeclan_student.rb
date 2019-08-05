f41e9ce385f7fe93c969bbf07afd352d6c80f2c7
Liliana Everett <everettliliana@gmail.com>
2019-08-05 17:38:46 +0100

    part A almost done

---
+++ part_a/codeclan_student.rb
@@ -0,0 +1,44 @@
+class CodeclanStudent
+
+# Getters shortcuts:
+attr_reader(:student_name, :cohort)
+attr_writer(:student_name, :cohort)
+
+
+def initialize(input_student_name, input_cohort)
+  @student_name = input_student_name
+  @cohort = input_cohort
+end
+
+# Getters:
+# def student_name()
+#   return @student_name
+# end
+#
+# def cohort()
+#   return @cohort
+# end
+
+
+# def student_name=(new_name)
+#   @student_name = new_name
+# end
+
+def student_talk()
+  return 'I can talk!'
+end
+
+class CodeclanStudent
+  attr_accessor :favourite_language
+end
+
+def say_favourite_language(favourite_language)
+  return 'I love #{favourite_language}'
+
+end
+
+
+
+
+
+end
---
+++ part_a/specs/codeclan_student_specs.rb
@@ -0,0 +1,53 @@
+require('minitest/autorun')
+require('minitest/rg')
+# require('pry')
+require_relative('../codeclan_student')
+
+class TestCodeclanStudent < MiniTest::Test
+
+
+
+
+def test_student_name__1
+  codeclan_student = CodeclanStudent.new('Suzanne', 'E33')
+  assert_equal('Suzanne', codeclan_student.student_name())
+end
+
+def test_cohort__1
+  codeclan_student = CodeclanStudent.new('Suzanne', 'E33')
+  assert_equal('E33', codeclan_student.cohort())
+end
+
+def test_student_name__2
+  codeclan_student = CodeclanStudent.new('Liliana', 'E34')
+  assert_equal('Liliana', codeclan_student.student_name())
+end
+
+def test_cohort__2
+  codeclan_student = CodeclanStudent.new('Liliana', 'E34')
+  assert_equal('E34', codeclan_student.cohort())
+end
+
+def test_set_student_name
+  codeclan_student = CodeclanStudent.new('Liliana', 'E34')
+  codeclan_student.student_name = 'Sally'
+  assert_equal('Sally', codeclan_student.student_name())
+end
+
+def test_student_talk__can
+  codeclan_student = CodeclanStudent.new('Liliana', 'E34')
+  codeclan_student.student_talk()
+  assert_equal('I can talk!', codeclan_student.student_talk())
+end
+
+def test_student_favourite_language
+  codeclan_student = CodeclanStudent.new('Suzanne', 'E33', 'Ruby')
+  codeclan_student.say_favourite_language()
+  assert_equal('I love Ruby', codeclan_student.favourite_language())
+end
+
+
+
+
+
+end
